87486d6c68ebd0bd6722cbf1c52785051ec59cbe
Nicholas B. Hubbard <nicholashubbard@posteo.net>
2026-06-29 15:08:12 -0400

    Fix ovn-host precondition check and controller connection status
    - Remove system-id from pre.sh preconditions since OVS sets it on startup
    - Use ovn-appctl instead of ovs-appctl for connection status check to
      find the pidfile in the correct OVN runtime directory

--- tests/self/ovn-host/pre.sh
+++ tests/self/ovn-host/pre.sh
@@ -7,7 +7,7 @@ source "$(dirname "$0")/../../lib/ovn.sh"
 assert_process_absent ovn-controller
 assert_ovs_bridge_absent br-int
 
-for key in ovn-remote ovn-encap-type ovn-encap-ip system-id; do
+for key in ovn-remote ovn-encap-type ovn-encap-ip; do
     assert_ovs_external_id_absent "$key"
 done
 
