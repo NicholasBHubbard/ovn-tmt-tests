4628a164ecd42b4be473841e5b38f45f0820e666
Nicholas B. Hubbard <nicholashubbard@posteo.net>
2026-06-30 09:12:00 -0400

    Rename roles to use underscores and install ansible.posix in lint CI
    ansible-lint requires role names to match ^[a-z][a-z0-9_]*$. Rename all
    five roles from hyphenated to underscored names and update all references
    in playbooks, include_role statements, and test assertions.
    
    Install ansible.posix collection in the lint CI job so ansible-lint can
    resolve the firewalld module.

--- tests/self/ansible-packaging/post.sh
+++ tests/self/ansible-packaging/post.sh
@@ -4,29 +4,29 @@ set -euo pipefail
 source "$(dirname "$0")/../../lib/assert.sh"
 cd_repo_root
 
-assert_file roles/distro-packages/defaults/main.yml
-assert_file roles/distro-packages/tasks/main.yml
+assert_file roles/distro_packages/defaults/main.yml
+assert_file roles/distro_packages/tasks/main.yml
 
-assert_contains roles/distro-packages/tasks/main.yml 'distro_package_names'
-assert_contains roles/distro-packages/tasks/main.yml 'ansible_facts["pkg_mgr"] == "apt"'
-assert_contains roles/distro-packages/tasks/main.yml 'ansible_facts["pkg_mgr"] in ["dnf", "dnf5", "yum"]'
+assert_contains roles/distro_packages/tasks/main.yml 'distro_package_names'
+assert_contains roles/distro_packages/tasks/main.yml 'ansible_facts["pkg_mgr"] == "apt"'
+assert_contains roles/distro_packages/tasks/main.yml 'ansible_facts["pkg_mgr"] in ["dnf", "dnf5", "yum"]'
 
-assert_contains roles/ovn-install/defaults/main.yml 'ovn_distro_package_names'
-assert_contains roles/ovn-install/defaults/main.yml 'ovn_distro_repository_package_names'
-assert_contains roles/ovs-setup/defaults/main.yml 'ovs_package_names'
-assert_contains roles/ovs-setup/defaults/main.yml 'ovs_repository_package_names'
+assert_contains roles/ovn_install/defaults/main.yml 'ovn_distro_package_names'
+assert_contains roles/ovn_install/defaults/main.yml 'ovn_distro_repository_package_names'
+assert_contains roles/ovs_setup/defaults/main.yml 'ovs_package_names'
+assert_contains roles/ovs_setup/defaults/main.yml 'ovs_repository_package_names'
 
-assert_not_contains roles/ovn-central/tasks/main.yml 'distro-packages'
-assert_not_contains roles/ovn-central/defaults/main.yml 'ovn_central_package_names'
-assert_not_contains roles/ovn-host/tasks/main.yml 'distro-packages'
-assert_not_contains roles/ovn-host/defaults/main.yml 'ovn_host_package_names'
+assert_not_contains roles/ovn_central/tasks/main.yml 'distro_packages'
+assert_not_contains roles/ovn_central/defaults/main.yml 'ovn_central_package_names'
+assert_not_contains roles/ovn_host/tasks/main.yml 'distro_packages'
+assert_not_contains roles/ovn_host/defaults/main.yml 'ovn_host_package_names'
 
-assert_contains playbooks/ovn-central.yml 'ovn-install'
-assert_contains playbooks/ovn-central.yml 'ovs-setup'
-assert_contains playbooks/ovn-host.yml 'ovn-install'
-assert_contains playbooks/ovn-host.yml 'ovs-setup'
-assert_contains playbooks/multihost.yml 'ovn-install'
-assert_contains playbooks/multihost.yml 'ovs-setup'
+assert_contains playbooks/ovn-central.yml 'ovn_install'
+assert_contains playbooks/ovn-central.yml 'ovs_setup'
+assert_contains playbooks/ovn-host.yml 'ovn_install'
+assert_contains playbooks/ovn-host.yml 'ovs_setup'
+assert_contains playbooks/multihost.yml 'ovn_install'
+assert_contains playbooks/multihost.yml 'ovs_setup'
 
 assert_not_contains playbooks 'centos-release-nfv-openvswitch'
 assert_not_contains playbooks 'Enable NFV SIG repo'
