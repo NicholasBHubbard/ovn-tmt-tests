83e4320af320b88355861e35915ea126d31e0164
Nicholas B. Hubbard <nicholashubbard@posteo.net>
2026-07-14 14:08:26 -0400

    Enable Werror across OVN CI plans

--- tests/self/ovn-install/post.sh
+++ tests/self/ovn-install/post.sh
@@ -4,4 +4,10 @@ set -euo pipefail
 source "$TMT_TREE/tests/lib/ovn.sh"
 
 assert_ovn_binaries_installed
+
+if [ "${EXPECT_WERROR:-false}" = true ]; then
+    assert_contains /usr/src/ovn/ovs/config.log "--enable-Werror"
+    assert_contains /usr/src/ovn/config.log "--enable-Werror"
+fi
+
 assert_finish
