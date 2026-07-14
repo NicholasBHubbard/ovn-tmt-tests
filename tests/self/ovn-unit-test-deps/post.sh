f91f0ecd85511193824fc02339b40463604bc944
Nicholas B. Hubbard <nicholashubbard@posteo.net>
2026-07-14 11:52:44 -0400

    Install missing OVN unit test tools

--- tests/self/ovn-unit-test-deps/post.sh
+++ tests/self/ovn-unit-test-deps/post.sh
@@ -5,6 +5,8 @@ source "$TMT_TREE/tests/lib/assert.sh"
 
 assert_command_present openssl
 assert_command_present pip3
+assert_command_present ps
+assert_command_present tcpdump
 
 for pkg in scapy pyftpdlib tftpy netaddr pyOpenSSL; do
     if ! pip3 show "$pkg" >/dev/null 2>&1; then
