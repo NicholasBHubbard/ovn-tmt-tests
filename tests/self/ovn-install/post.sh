16b3a75db8407c2e1717e86acedc36e87e47d497
Nicholas B. Hubbard <nicholashubbard@posteo.net>
2026-07-16 09:43:35 -0400

    Support shallow checkout by commit

--- tests/self/ovn-install/post.sh
+++ tests/self/ovn-install/post.sh
@@ -5,6 +5,9 @@ source "$TMT_TREE/tests/lib/ovn.sh"
 
 assert_ovn_binaries_installed
 
+assert_contains "$TMT_TREE/roles/ovn_install/tasks/git.yml" \
+    'refspec: "+{{ ovn_git_version }}:refs/ovn-tmt/{{ ovn_git_version }}"'
+
 if [ "${EXPECT_WERROR:-false}" = true ]; then
     assert_contains /usr/src/ovn/ovs/config.log "--enable-Werror"
     assert_contains /usr/src/ovn/config.log "--enable-Werror"
